ea168e6dc82978ad2530b3ec1c60425acf82324f
dariakhanuichenko <deyadecember@gmail.com>
2019-08-20 13:08:26 +0300

    add UserDao(user Jpa Repository) add RoleDao(user Jpa Repository)
    add interface UserService
    add class UserServiceImpl ( implements UserService) with methods save() and findByUsername()

---
+++ src/main/java/net/proselyte/springsecurityapp/dao/UserDao.java
@@ -0,0 +1,9 @@
+package net.proselyte.springsecurityapp.dao;
+
+import net.proselyte.springsecurityapp.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface UserDao extends JpaRepository<User,Long> {
+    User findByUsername(String username);
+}
